4e5654d83e670d4ec55162d6ac1c7718aaac6b66
matthew <matthewbudiman@outlook.com>
2021-06-06 18:45:55 -0500

    init

---
+++ pages/working.js
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import * as THREE from "three";
+
+export default function Working() {
+  useEffect(() => {
+    const canvas = document.querySelector("#canvasArea");
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    camera.position.z = 5;
+
+    const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    const geo = new THREE.BoxGeometry();
+    const normalMat = new THREE.MeshNormalMaterial();
+    const cube = new THREE.Mesh(geo, normalMat);
+    cube.name = "theCube";
+    scene.add(cube);
+
+    const raycaster = new THREE.Raycaster();
+    const mousePos = new THREE.Vector2();
+
+    window.addEventListener("click", (event) => {
+      event.preventDefault();
+      mousePos.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mousePos.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      raycaster.setFromCamera(mousePos, camera);
+      const items = raycaster.intersectObjects(scene.children, true);
+
+      if (items.length > 0 && items[0].object) {
+        if (items[0].object.name === "theCube") {
+          console.log(items[0].object);
+          alert("Cube cliked");
+        }
+
+        console.log(items[0].object.name);
+      }
+    });
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  }, []);
+
+  return <canvas id="canvasArea"></canvas>;
+}
